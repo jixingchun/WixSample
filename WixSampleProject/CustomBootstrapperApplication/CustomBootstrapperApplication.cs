1454a412edfb099878b7f4490acaa3f0448e2af4
jxc <jixingchun@hotmail.com>
2019-06-02 21:01:11 +0800

    添加自定义引导程序

---
+++ WixSampleProject/CustomBootstrapperApplication/CustomBootstrapperApplication.cs
@@ -0,0 +1,30 @@
+﻿using CustomBA.Models;
+using CustomBA.ViewModels;
+using CustomBA.Views;
+using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+
+namespace CustomBA
+{
+    public class CustomBootstrapperApplication : BootstrapperApplication
+    {
+        public static Dispatcher Dispatcher { get; set; }
+        protected override void Run()
+        {
+            Dispatcher = Dispatcher.CurrentDispatcher;
+            var model = new BootstrapperApplicationModel(this);
+            var viewModel = new InstallViewModel(model);
+            var view = new InstallView(viewModel);
+            model.SetWindowHandle(view);
+            this.Engine.Detect();
+            view.Show();
+            Dispatcher.Run();
+            this.Engine.Quit(model.FinalResult);
+        }
+    }
+}
