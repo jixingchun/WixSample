a75baa4685240e19966f921bf1803262005863e2
jxc <jixingchun@hotmail.com>
2019-06-09 22:11:43 +0800

    添加Wix BA

---
+++ WixSampleProject/BA/NewsItem.cs
@@ -0,0 +1,18 @@
+
+
+namespace ProjectPack.BA
+{
+    using System;
+
+    /// <summary>
+    /// The model for an individual news item.
+    /// </summary>
+    public class NewsItem
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public string Snippet { get; set; }
+        public DateTime Updated { get; set; }
+    }
+}
