3622f6f85cf5ca74f7e98cdba9ba0900b13c6084
jxc <jixingchun@deltaww.com>
2019-06-11 10:19:31 +0800

    添加自定义BA

---
+++ WixSampleProject/CustomBA/CustomBootstrapperApplication.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Tools.WindowsInstallerXml.Bootstrapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Threading;
+
+namespace CustomBA
+{
+    class WixBA : BootstrapperApplication
+    {
+        public static Dispatcher Dispatcher { get; set; }
+
+        protected override void Run()
+        {
+            var view = new RootView();
+            view.Show();
+            Dispatcher.Run();
+        }
+    }
+}
