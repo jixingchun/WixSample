e3636d876659bcd42587d04e2c4dac6827f460c0
jxc <jixingchun@deltaww.com>
2019-07-10 19:09:30 +0800

    update

---
+++ WixSampleProject/ConsoleApp1/MSIEditor.cs
@@ -0,0 +1,100 @@
+﻿using System.Linq;
+
+namespace DIAViewSetup.Runtime.Preprocessing
+{
+    using Microsoft.Deployment.Compression;
+    using Microsoft.Deployment.WindowsInstaller;
+    using ProjectPack.Common;
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Text;
+
+    public class MSIEditor
+    {
+        /// <summary>
+        /// 把运行时嵌入到MSI文件中
+        /// </summary>
+        /// <param name="diaviewDir"></param>
+        /// <returns></returns>
+        public static bool EmbedRuntimeFile(string runtimeMsiFile, string diaviewDir)
+        {
+            // 嵌入RuntimeFiles
+            string runtimeFileName = "RuntimeFileList.txt";
+
+            List<string> files = File.ReadAllLines(runtimeFileName,Encoding.UTF8).ToList();
+            string tempDir = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString());
+            Directory.CreateDirectory(tempDir);
+            foreach(string file in files)
+            {
+                string scrFile = Path.Combine(diaviewDir, file);
+                string desFile = Path.Combine(tempDir, file);
+
+                FileUtils.CopyFile(scrFile, desFile, false);
+            }
+
+            string  tempFile = Path.GetTempFileName();
+            ArchiveInfo runtimeFileAchiveInfo = new Microsoft.Deployment.Compression.Zip.ZipInfo(tempFile);
+            runtimeFileAchiveInfo.Pack(tempDir, true, CompressionLevel.Max, null);
+            FileUtils.DeleteDirectory(tempDir);
+
+            using (Database db = new Database(runtimeMsiFile, DatabaseOpenMode.Direct))
+            {
+                using (View view = db.OpenView("INSERT INTO `Binary` (`Name`, `Data`) VALUES(?, ?)"))
+                {
+                    using (Record record = new Record(2))
+                    {
+                        record[1] = "RuntimeFiles";
+                        record.SetStream(2, tempFile);
+                        view.Execute(record);
+                    }
+                }
+                db.Commit();
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// 把用户工程嵌入到MSI文件中
+        /// </summary>
+        /// <param name="projectFile"></param>
+        /// <param name="lan"></param>
+        /// <returns></returns>
+        public static bool EmbedProjectFile(string msiFile, string projectFile, string lan)
+        {
+            // 生成ZIP包
+            string tempFile = Path.GetTempFileName();
+            string projectFileDir = Path.GetDirectoryName(projectFile);
+            ArchiveInfo archiveInfo = new Microsoft.Deployment.Compression.Zip.ZipInfo(tempFile);
+            archiveInfo.Pack(projectFileDir, true, CompressionLevel.Max, null);
+
+            // 把ZIP文件写入 MSI文件中
+            using (Database db = new Database(msiFile, DatabaseOpenMode.Direct))
+            {
+                using (View view = db.OpenView("INSERT INTO `Binary` (`Name`, `Data`) VALUES(?, ?)"))
+                {
+                    using (Record record = new Record(2))
+                    {
+                        record[1] = "UserProject";
+                        record.SetStream(2, tempFile);
+                        view.Execute(record);
+                    }
+                }
+
+                using (View view2 = db.OpenView("INSERT INTO `Property` (`Property`, `Value`) VALUES (?, ?)"))
+                {
+                    using (Record record = new Record(2))
+                    {
+                        record[1] = "SetupLanguage";
+                        record[2] = lan;
+                        view2.Execute(record);
+                    }
+                }
+
+                db.Commit();
+            }
+            return true;
+        }
+    }
+}
--- WixSampleProject/ConsoleApp1/Program.cs
+++ WixSampleProject/ConsoleApp1/Program.cs
@@ -1,55 +1,264 @@
-﻿using System;
+﻿using Microsoft.Deployment.WindowsInstaller;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Xml;
+using System.Windows;
 
-namespace ConsoleApp1
+namespace DIAViewSetup.Runtime.Preprocessing
 {
     class Program
     {
         static void Main(string[] args)
         {
 
-            XmlDocument docVar = new XmlDocument();
-            docVar.Load("AppVariables.wxi");
-            XmlNode includeNode = docVar.SelectSingleNode("Include");
-            foreach (XmlNode item in includeNode.ChildNodes)
+            string msiBaseDir = @"D:\work\Source\Common\Trial\Xingchun.ji\ProjectPack.V3\DIAViewSetup.Runtime.V3\bin\Debug";
+
+            string msi_en_US = Path.Combine(msiBaseDir, @"en-us\DIAViewSetup.Runtime.msi");
+            string msi_en = Path.Combine(msiBaseDir, @"en\DIAViewSetup.Runtime.msi");
+            string msi_zh_CN = Path.Combine(msiBaseDir, @"zh-cn\DIAViewSetup.Runtime.msi");
+            string msi_zh_CHT = Path.Combine(msiBaseDir, @"zh-tw\DIAViewSetup.Runtime.msi");
+
+            Dictionary<string, string> dicLanFiles = new Dictionary<string, string>()
             {
-                if (item.Name.Equals("define") && item.Value.StartsWith("SourceFileDir"))
-                {
-                    item.Value = @"SourceFileDir = ""C:\Program Files (x86)\DIAView\""";
-                    break;
-                }
-            }
-            docVar.Save("AppVariables.wxi");
+                { "en-US",msi_en_US},
+                { "en",msi_en},
+                { "zh-CN",msi_zh_CN},
+                { "zh-CHT",msi_zh_CHT},
+            };
 
+            LanguageTransService service = new LanguageTransService();
+            bool ret = service.Excute(dicLanFiles);
 
-            
-
-
-            XmlDocument doc = new XmlDocument();
-            doc.Load("Bundle.wxs");
-
-
-            XmlNodeList msiPackageList = doc.GetElementsByTagName("MsiPackage");
-
-
-            foreach (XmlNode xmlNode in msiPackageList)
+            if (!ret)
             {
-                string attrId = xmlNode.Attributes["Id"].Value;
-
-                if ("RuntimeSetup.msi".Equals(attrId,StringComparison.OrdinalIgnoreCase))
-                {
-                    xmlNode.Attributes["SourceFile"].Value = "x";
-                }
-                else if ("UserProjectSetup.msi".Equals(attrId, StringComparison.OrdinalIgnoreCase))
-                {
-                    xmlNode.Attributes["SourceFile"].Value = "y";
-                }
+                MessageBox.Show("处理失败！");
             }
 
-            doc.Save("Bundle.wxs");
+            //service.SetupTest(dicLanFiles);
+
+            //ForTest();
+        }
+
+
+        private static void ForTest()
+        {
+            string msiBaseDir = @"D:\work\Source\Common\Trial\Xingchun.ji\ProjectPack.V3\DIAViewSetup.Runtime.V3\bin\Debug";
+
+            string msiFile = Path.Combine(msiBaseDir, @"en-us\DIAViewSetup.Runtime.msi");
+            string msiFile2 = Path.Combine(msiBaseDir, @"en-us\DIAViewSetup.Runtime.copy.msi");
+            File.Copy(msiFile, msiFile2, true);
+
+            string diaViwe = @"C:\Program Files (x86)\DIAView";
+
+            string projectFile = @"D:\work\DiaViewProject\DemoProject\DemoProject260.project";
+            string lan = "zh-CN";
+
+            MSIEditor.EmbedRuntimeFile(msiFile2, diaViwe);
+            MSIEditor.EmbedProjectFile(msiFile2, projectFile, lan);
+        }
+
+        private void SetShortCutForTest()
+        {
+            string msiBaseDir = @"D:\work\Source\Common\Trial\Xingchun.ji\ProjectPack.V3\DIAViewSetup.Runtime.V3\bin\Debug";
+
+            string msi_en_US = Path.Combine(msiBaseDir, @"en-us\DIAViewSetup.Runtime.msi");
+            string msi_en = Path.Combine(msiBaseDir, @"en\DIAViewSetup.Runtime.msi");
+            string msi_zh_CN = Path.Combine(msiBaseDir, @"zh-cn\DIAViewSetup.Runtime.msi");
+            string msi_zh_CHT = Path.Combine(msiBaseDir, @"zh-tw\DIAViewSetup.Runtime.msi");
+
+            string msiFile2 = Path.Combine(msiBaseDir, @"en-us\DIAViewSetup.Runtime.copy.msi");
+            using (Database db = new Database(msiFile2, DatabaseOpenMode.Direct))
+            {
+                string sql = db.Tables["Shortcut"].SqlSelectString + " WHERE `Shortcut` = 'DesktopShortcut'";
+                string updateSql = "UPDATE `Shortcut` SET `Description` = 'xxxyyy' WHERE `Shortcut` = 'DesktopShortcut'";
+
+                db.Execute(updateSql);
+
+
+                using (View view3 = db.OpenView(sql))
+                {
+                    IList<TableInfo> viewTables = view3.Tables;
+                    string sql2 = view3.Tables[0].SqlSelectString + " WHERE `Shortcut` = 'DesktopShortcut'";
+
+
+                }
+
+                db.Commit();
+                return;
+            }
+        }
+
+
+
+        public class LanguageTransService
+        {
+            private string WixToolPath = @"C:\Program Files (x86)\WiX Toolset v3.11\bin";
+
+            public bool Excute(Dictionary<string, string> dicFiles)
+            {
+                if (LanguageTransfor(dicFiles))
+                {
+                    //return EmbedLanguageResource(dicFiles);
+                    return true;
+                }
+                return false;
+            }
+
+            /// <summary>
+            /// 生成变形文件
+            /// </summary>
+            private bool LanguageTransfor(Dictionary<string, string> dicFiles)
+            {
+                if (dicFiles == null || dicFiles.Count <= 1) return false;
+
+                // 准备torch命令
+                string torchExe = Path.Combine(WixToolPath, "torch.exe");
+                string baseName = dicFiles.ElementAt(0).Key;
+                string baseMsiFile = dicFiles.ElementAt(0).Value;
+
+                for(int i=1; i< dicFiles.Count; i++)
+                {
+                    string toName = dicFiles.ElementAt(i).Key;
+                    string toMsiFile = dicFiles.ElementAt(i).Value;
+
+                    string mstFile = $"transforms\\{toName}.mst";
+
+                    string torchCmd = $"-t language {baseMsiFile} {toMsiFile} -out {mstFile}";
+
+                    bool ret = ProcessUtils.RunWixCommand(torchExe, torchCmd, Directory.GetCurrentDirectory());
+                    if (!ret) return false;
+                }
+                return true;
+            }
+
+            /// <summary>
+            /// 嵌入资源文件到MSI包
+            /// </summary>
+            private bool EmbedLanguageResource(Dictionary<string, string> dicFiles)
+            {
+                string baseName = dicFiles.ElementAt(0).Key;
+                string baseMsiFile = dicFiles.ElementAt(0).Value;
+
+                for (int i = 1; i < dicFiles.Count; i++)
+                {
+                    string toName = dicFiles.ElementAt(i).Key;
+                    string toMsiFile = dicFiles.ElementAt(i).Value;
+
+                    string mstFile = $"transforms\\{toName}.mst";
+
+                    bool ret = EmberdFile(baseMsiFile, mstFile);
+                    if (!ret) return false;
+                }
+                return true;
+            }
+
+            private bool EmberdFile(string msiFile, string transformFile)
+            {
+                using (Database db = new Database(msiFile, DatabaseOpenMode.Direct))
+                {
+                    using (View view = db.OpenView("INSERT INTO `_Storages` (`Name`, `Data`) VALUES(?, ?)"))
+                    {
+                        using (Record record = new Record(2))
+                        {
+                            record[1] = Path.GetFileName(transformFile);
+                            record.SetStream(2, transformFile);
+                            view.Execute(record);
+                        }
+                    }
+                    db.Commit();
+                }
+                return true;
+            }
+
+            /// <summary>
+            /// 测试安装包
+            /// </summary>
+            public void SetupTest(Dictionary<string, string> dicFiles)
+            {
+                string baseName = dicFiles.ElementAt(0).Key;
+                string baseMsiFile = dicFiles.ElementAt(0).Value;
+
+                for (int i = 0; i < dicFiles.Count; i++)
+                {
+                    string toName = dicFiles.ElementAt(i).Key;
+                    string args = $"/i  {baseMsiFile} TRANSFORMS=:{toName}.mst";
+
+                    ProcessUtils.RunWixCommand("msiexec", args, Directory.GetCurrentDirectory());
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+
+
+    public class ProcessUtils
+    {
+        private static string DefaultWorkingDir = Path.Combine(Path.GetTempPath(), "DiaviewPack");
+
+        /// <summary>
+        /// 执行Wix相关命令
+        /// </summary>
+        /// <param name="wixExe"></param>
+        /// <param name="para"></param>
+        /// <param name="defaultWorkingDir"></param>
+        /// <returns></returns>
+        public static bool RunWixCommand(string wixExe, string para, string workingDir = "")
+        {
+            if (string.IsNullOrEmpty(workingDir))
+            {
+                workingDir = DefaultWorkingDir;
+            }
+
+            var wixProcess = new Process
+            {
+                StartInfo =
+                {
+                    FileName = wixExe,
+                    UseShellExecute = false,
+                    RedirectStandardInput = true,
+                    RedirectStandardOutput = true,
+                    RedirectStandardError = true,
+                    CreateNoWindow = true,
+                    WorkingDirectory = workingDir,
+                    Arguments=para
+                },
+            };
+
+            Stopwatch sw = new Stopwatch();
+
+            try
+            {
+                string cmd = $"{wixExe} {para}";
+                
+                sw.Start();
+
+                wixProcess.Start();
+                wixProcess.WaitForExit();
+                int exitCode = wixProcess.ExitCode;
+                if (exitCode != 0)
+                {
+                    string output = wixProcess.StandardOutput.ReadToEnd();
+                    throw new Exception(output);
+                }
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            finally
+            {
+                if (wixProcess != null)
+                {
+                    wixProcess.Close();
+                }
+                sw.Stop();
+
+                sw = null;
+            }
         }
     }
 }
